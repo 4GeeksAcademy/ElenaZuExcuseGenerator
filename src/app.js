3240051678e3c6cc067c27ea1fc9c5039698ac10
ElenaZu <128751514+ElenaZu@users.noreply.github.com>
2023-06-15 02:32:49 +0000

    Más cambios

--- src/app.js
+++ src/app.js
@@ -11,17 +11,18 @@ let when = [
 
 window.onload = function() {
   var randomNumber = Math.floor(Math.random() * 4);
-
   var randomNumber2 = Math.floor(Math.random() * 3);
+  var randomNumber3 = Math.floor(Math.random() * 4);
+  var randomNumber4 = Math.floor(Math.random() * 4);
 
   let excuse =
     who[randomNumber] +
     " " +
-    action[randomNumber] +
+    action[randomNumber3] +
     " " +
     what[randomNumber2] +
     " " +
-    when[randomNumber];
+    when[randomNumber4];
   let answer = document.getElementById("excuse");
   answer.innerText = excuse;
 };
